56daf588154e99ab1cc5fe13aa404f42311b55c6
Kehinde Bello <adedotunomomeji@gmail.com>
2025-02-11 11:46:19 +0100

    check: that deployment updates

--- api/routes/books.py
+++ api/routes/books.py
@@ -47,6 +47,10 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+@router.get("/health", status_code=status.HTTP_200_OK)
+async def health_check():
+    return {"message": "FastAPI is running!"}
+
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int) -> Book:
     book = db.get_book(book_id)
