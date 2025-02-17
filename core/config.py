9dc03b6e4fc072d0aacb9e64278fb66c434ccb8c
Kehinde Bello <adedotunomomeji@gmail.com>
2025-02-17 09:03:42 +0100

    chore: env setup

--- core/config.py
+++ core/config.py
@@ -1,4 +1,7 @@
 import secrets
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from pydantic_settings import BaseSettings
 
@@ -11,6 +14,10 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     DEBUG: bool = False
     TESTING: bool = False
+    EC2_IP: str = os.getenv("EC2_IP", "34.228.162.152")
+    TICK_URL: str = f"http://{EC2_IP}.compute-1.amazonaws.com/api/v1/test/telex-webhook"
+    SLACK_WEBHOOK_URL: str = os.getenv("SLACK_WEBHOOK_URL", "")
+    APP_URL: str = f"http://{EC2_IP}.compute-1.amazonaws.com"
 
 
 settings = Settings()
